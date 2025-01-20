3465654fbfd37c86ddbd0e2d163b4af2c18d9fbb
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2025-01-20 12:37:40 +0300

    Project Structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,29 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-cyan-800 py-2">
+      <div className="container mx-auto px-4 flex justify-between">
+        {/* Logo */}
+        <div>
+          <a href="/" className="text-2xl font-bold cursor-pointer">
+            E-commerce
+          </a>
+        </div>
+
+        {/* Links */}
+
+        <div>
+          <ul className="flex justify-between gap-6">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Category</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
